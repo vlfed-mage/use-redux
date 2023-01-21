4d966500cfea7627d3273943ea9b43a25d86f965
Valdo <gkorchun@gmail.com>
2023-01-21 16:13:11 +0200

    Simple UI useing Redux

--- src/index.js
+++ src/index.js
@@ -10,15 +10,31 @@ const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case 'INC':
 			return { ...state, value: state.value + 1 };
+		case 'DEC':
+			return { ...state, value: state.value - 1 };
 		default:
 			return state;
 	}
 };
 
 const store = createStore(reducer);
-store.subscribe(() => {
-	console.log(store.getState());
-});
 
-store.dispatch({ type: 'INC' });
-store.dispatch({ type: 'INC' });
+document
+	.getElementById('inc')
+	.addEventListener('click', () => {
+		store.dispatch({ type: 'INC' })
+	});
+
+document
+	.getElementById('dec')
+	.addEventListener('click', () => {
+		store.dispatch({ type: 'DEC' })
+	})
+
+const update = () => {
+	document
+		.getElementById('counter')
+		.innerHTML = store.getState().value;
+}
+
+store.subscribe(update)
