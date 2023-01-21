83daaf242c55474462e9ae8f677cb4ef3ab19370
Valdo <gkorchun@gmail.com>
2023-01-21 16:27:06 +0200

    Extra ations' params

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createStore } from "redux";
 
 const initialState = {
 	value: 0
-}
+};
 
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
@@ -12,6 +12,8 @@ const reducer = (state = initialState, action) => {
 			return { ...state, value: state.value + 1 };
 		case 'DEC':
 			return { ...state, value: state.value - 1 };
+		case 'RND':
+			return { ...state, value: state.value + action.payload };
 		default:
 			return state;
 	}
@@ -29,12 +31,22 @@ document
 	.getElementById('dec')
 	.addEventListener('click', () => {
 		store.dispatch({ type: 'DEC' })
-	})
+	});
+
+document
+	.getElementById('rnd')
+	.addEventListener('click', () => {
+		const payload = Math.floor(Math.random()*10+1); // extra param for action
+		store.dispatch({
+		 	type: 'RND',
+			payload
+		})
+	});
 
 const update = () => {
 	document
 		.getElementById('counter')
 		.innerHTML = store.getState().value;
-}
+};
 
-store.subscribe(update)
+store.subscribe(update);
